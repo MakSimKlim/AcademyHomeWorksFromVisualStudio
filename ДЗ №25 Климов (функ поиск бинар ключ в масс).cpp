e8ea0b744c9a53dc641a952256c29e017196e25b
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-02-26 17:51:28 +0500

    Add files via upload

---
+++ ДЗ №25 Климов (функ поиск бинар ключ в масс).cpp
@@ -0,0 +1,125 @@
+﻿// Задание #2
+// 
+// ДЗ №25 Климов (функ поиск бинар ключ в масс)
+//
+//Написать функцию, реализующую алгоритм
+//бинарного поиска заданного ключа в одномерном массиве.
+
+
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int ArrRandomise(int arr[], const int size)    // функция заполнения массива рандомными значениями
+{
+    for (int i = 0; i < size; i++)             
+    {                                          
+        arr[i] = 1 + rand() % (10 - 1);        // заполняем массив рандомайзером
+        cout << arr[i] << " ";                 // Вывод неотсортированного массива
+    }
+    cout << "\n";
+
+    return arr[size];
+}
+
+int SortMassChoiseMethod(int arr[], const int size)    // функция сортировки массива методом выбора
+{
+    int index_min = 0;                         // переменная для минимального значения
+    int temp;                                  // переменная - буфер, нужная при сортировке массива
+    for (int i = 0; i < size - 1; i++)         // сортируем массив методом выбора
+    {
+        index_min = i;                         // за минимальный берем первый из неотсортированных
+        for (int j = i + 1; j < size; j++)     // начинаем проверять с последующего за ним элемента все оставшиеся
+        {
+            if (arr[j] < arr[index_min])       // если какой-то из них меньше...
+            {
+                index_min = j;                 //... то запоминаем его номер
+            }
+
+        }
+        if (index_min != i)                    // если в ходе проверки индекс минимального элемента поменялся
+        {
+            temp = arr[i];                     // то меняем местами элементы
+            arr[i] = arr[index_min];
+            arr[index_min] = temp;
+        }
+    }
+   // Вывод отсортированного массива
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return arr[size];
+}
+
+int BinarySearch(int arr[],const int size, int elem)  // функция бинарного поиска
+{
+    int notSearch = -1;
+    int key = 0;
+    int start = 0, end = size - 1;       // задаем начало и конец массива для поиска
+    int middle = 0;
+
+    while (start < end)
+    {
+        middle = (start + end) / 2;
+
+        if (elem == arr[middle])
+        {
+            key = middle;
+            break;
+        }
+        if (elem < arr[middle])
+        {
+            end = middle - 1;
+        }
+        else
+        {
+            start = middle + 1;
+        }
+
+
+    }
+    if (middle + 1 > end || middle - 1 < start)
+    {
+        cout << "Элемент не найден!";
+    }
+    else
+    {
+        cout << "Элемент найден! Порядковый номер в массиве №" << key+1;
+            
+    }
+
+    return elem;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
+
+    cout << "\n **********************************************************************" << endl;
+    cout << " *Программа бинарного поиска заданного ключа в одномерном массиве v1.0*" << endl;
+    cout << " **********************************************************************" << endl;
+
+
+    const int arrSize = 10;
+    int arr[arrSize];
+
+    ArrRandomise(arr, arrSize);                // функция создания рандомного массива
+
+    SortMassChoiseMethod(arr, arrSize);        // функция сортировки массива методом выбора
+
+    int searchElem = 0;
+
+    cout << "Введите число для поиска ключа в массиве" << "\n";
+    cin >> searchElem;
+
+    BinarySearch(arr, arrSize, searchElem);  // функция бинарного поиска
+                                               // не работает без предварительной сортировки
+
+    cout << "\n";
+    return 0;
+}
---
+++ ДЗ №25 Климов (функция линейн поиск ключ в масс).cpp
@@ -0,0 +1,60 @@
+﻿// Задание #1
+// 
+// ДЗ №25 Климов (функция линейного поиск ключа в массиве)
+//
+//Написать функцию, реализующую алгоритм
+//линейного поиска заданного ключа в одномерном массиве.
+
+
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int LinearSearch(int array[], int size, int key)//нач знач, массив, размер знач
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] == key)
+        {
+            return i;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
+
+    cout << "\n **********************************************************************" << endl;
+    cout << " *Программа линейного поиска заданного ключа в одномерном массиве v1.0*" << endl;
+    cout << " **********************************************************************" << endl;
+
+    const int arraySize = 20;
+    int a[arraySize], searchKey, element;
+
+    for (int x = 0; x < arraySize; x++)
+    {
+        a[x] = 1 + rand() % (10 - 1);
+        cout << a[x] << " ";
+    }
+
+    cout << "\nВведите ключ: ";
+    cin >> searchKey;
+
+    element = LinearSearch(a, arraySize, searchKey);
+
+    if (element != arraySize)
+    {
+        cout << "Элемент найден: " << element + 1 << "\n";
+            
+    }
+    else
+    {
+        cout << "\n Значение не найдено";
+    }
+
+    return 0;
+
+}
