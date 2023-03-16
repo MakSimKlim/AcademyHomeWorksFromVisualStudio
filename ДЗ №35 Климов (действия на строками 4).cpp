6d7f13a783b5c0c5c4ec3eba99a9e4c746431735
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-03-16 22:55:01 +0500

    Add files via upload

---
+++ ДЗ №35 Климов (действия на строками 4).cpp
@@ -0,0 +1,65 @@
+﻿//// Задание #4
+// 
+// ДЗ №35 Климов (действия на строками 4)
+//
+//Написать программу, которая заменяет все
+//символы точки «.» в строке, введенной пользователем,
+//
+
+
+#include <iostream>
+#include <time.h>
+#include <Windows.h>
+#include <iomanip>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+
+void ReplaceChar(string& str, char simbol, char simbol2)//функция которая вставляет в строку в указанную позицию заданный символ.
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == simbol)
+        {
+            str[i] = simbol2;
+        }
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    srand(time(NULL));
+
+    cout << "\n **********************************************************************************" << endl;
+    cout << " *             Программа которая заменяет все символы точки «.» в строке,         *" << endl;
+    cout << " *        введенной пользователем на символы восклицательного знака «!». V1.1     *" << endl;
+    cout << " **********************************************************************************" << endl;
+
+    const int size = 20;
+    char str[size];
+    int num = 0;
+    char simbol = '.';
+    char simbol2= '!';
+
+
+    cout << "Введите строку:" << "\n";
+    cin.get(str, size);//метод get - 1 аргумент массив пользователя, 2- размерность    
+
+
+
+    string str2 = str;
+
+
+    ReplaceChar(str2, simbol, simbol2);//функция которая вставляет в строку в указанную позицию заданный символ.
+
+
+    cout << "Строка с замененными символами: " << str2 << "\n";
+
+
+    return 0;
+}
---
+++ ДЗ №35 Климов (действия над строками №5).cpp
@@ -0,0 +1,59 @@
+﻿//// Задание #5
+// 
+// ДЗ №35 Климов (действия над строками5)
+//
+//Пользователь вводит строку символов и искомый символ, 
+//посчитать сколько раз он встречается 
+//в строке.
+
+
+#include <iostream>
+#include <time.h>
+#include <Windows.h>
+#include <iomanip>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+void CountChar(string& str, char simbol, int j)//функция которая считает количество заданных символов в строке.
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == simbol)
+        {
+            j++;
+        }
+    }
+    cout << "Символ " << simbol << " встречается " << j << " раз " << "\n";
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    srand(time(NULL));
+
+    cout << "\n **********************************************************************************" << endl;
+    cout << " *      Программа подсчета введенных пользователем символов в строке V1.1         *" << endl;
+    cout << " **********************************************************************************" << endl;
+
+    const int size = 20;
+    char str[size];
+    int num = 0;
+    char simbol;
+    int sum = 0;
+
+    cout << "Введите строку:" << "\n";
+    cin.get(str, size);//метод get - 1 аргумент массив пользователя, 2- размерность   
+
+    cout << "Введите символ:" << "\n";
+    cin >> simbol;
+
+    string str2 = str;
+
+    CountChar(str2, simbol, sum);
+
+    return 0;
+}
