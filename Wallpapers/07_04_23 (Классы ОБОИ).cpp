1b09d31f018cdc242c8d6dca73f1796b0b9b46e6
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-04-14 20:47:56 +0500

    Add files via upload

--- Wallpapers/07_04_23 (Классы ОБОИ).cpp
+++ Wallpapers/07_04_23 (Классы ОБОИ).cpp
@@ -8,6 +8,13 @@
 
 #include <iostream>
 #include <Windows.h>
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
 #include "Header.h"
 #include "Source.cpp"
 
