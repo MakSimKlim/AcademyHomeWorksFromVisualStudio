359a9c743f7d3815e9017c3fc93d4efdb76ee970
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-04-14 21:44:31 +0500

    Add files via upload

--- Wallpapers/07_04_23 (Классы ОБОИ).cpp
+++ Wallpapers/07_04_23 (Классы ОБОИ).cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <Windows.h>
+#include <stdlib.h>
+
 #include "mysql_connection.h"
 
 #include <cppconn/driver.h>
@@ -20,9 +22,54 @@
 
 using namespace std;
 
+const string server = "tcp://127.0.0.1:3306";
+const string username = "root";
+const string password = "";
+
 int main()
 {
+
     setlocale(LC_ALL, "Rus");
+
+    sql::Driver* driver;
+    sql::Connection* con;
+    sql::Statement* stmt;
+    sql::ResultSet* res;
+
+    try 
+    {
+        driver = get_driver_instance();
+        //con = driver->connect("tcp://127.0.0.1:3306", "root", " ");
+        con = driver->connect(server, username, password);
+
+        con->setSchema("test");
+
+        stmt = con->createStatement();
+        res = stmt->executeQuery("select * from `catalogwallpaper`");
+
+        while (res->next()) {
+            cout << "\t... MySQL replies: ";
+            /* Access column data by alias or column name */
+            cout << res->getString("nameMark") << endl;
+            cout << "\t... MySQL says it again: ";
+            /* Access column data by numeric offset, 1 is the first column */
+            cout << res->getString(1) << endl;
+        }
+        delete res;
+        delete stmt;
+        delete con;
+
+    }
+    catch (sql::SQLException& e) 
+    {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+    
+    cout << endl;
+    }
 
     cout << "Расчет обоев версия 1.0" << "\n";
     int inputCountRoom = 0;
@@ -43,6 +90,8 @@ int main()
     }
 
     //double paramsWallpaper[7][3] = {};
+    
+    //return EXIT_SUCCESS;
 
 }
 
