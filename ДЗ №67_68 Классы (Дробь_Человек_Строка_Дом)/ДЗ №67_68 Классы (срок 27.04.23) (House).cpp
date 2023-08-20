75d47f4f31e39adda1cb78b0d5980d48e0a1eb83
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-08-21 01:55:22 +0500

    Add files via upload

---
+++ ДЗ №67_68 Классы (Дробь_Человек_Строка_Дом)/ДЗ №67_68 Классы (срок 27.04.23) (House).cpp
@@ -0,0 +1,150 @@
+﻿// House
+//
+//Создайте программу, имитирующую многоквартирный дом.
+//Необходимо иметь классы Человек, Квартира, Дом.
+//Класс Квартира содержит динамический массив объектов класса Человек.
+//Класс Дом содержит массив объектов класса Квартира.Каждый из классов содержит
+//переменные - члены и функции - члены, которые необходимы для предметной области класса.
+//Обращаем ваше внимание, что память под строковые значения
+//выделяется динамически.Не забывайте обеспечить
+//классы различными конструкторами(конструктор
+//копирования обязателен), деструкторами
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+    class Person {
+    public:
+        Person(const string& name, int age) : name(new string(name)), age(age) {}
+
+        // Конструктор копирования
+        Person(const Person& other) : name(new string(*other.name)), age(other.age) {}
+
+        // Деструктор
+        ~Person() {
+            delete name;
+        }
+
+        void display() const {
+            cout << "Имя: " << *name << ", Возраст: " << age << endl;
+        }
+
+    private:
+        string* name;
+        int age;
+    };
+
+    class Apartment {
+    public:
+        Apartment(int number, int maxOccupants) : number(number), maxOccupants(maxOccupants), occupantsCount(0) {
+            occupants = new Person * [maxOccupants];
+        }
+
+        // Конструктор копирования
+        Apartment(const Apartment& other) : number(other.number), maxOccupants(other.maxOccupants), occupantsCount(other.occupantsCount) {
+            occupants = new Person * [maxOccupants];
+            for (int i = 0; i < occupantsCount; ++i) {
+                occupants[i] = new Person(*other.occupants[i]);
+            }
+        }
+
+        void addPerson(const Person& person) {
+            if (occupantsCount < maxOccupants) {
+                occupants[occupantsCount] = new Person(person);
+                occupantsCount++;
+            }
+        }
+
+        void display() const {
+            cout << "Номер кватиры: " << number << endl;
+            for (int i = 0; i < occupantsCount; ++i) {
+                occupants[i]->display();
+            }
+        }
+
+        // Деструктор
+        ~Apartment() {
+            for (int i = 0; i < occupantsCount; ++i) {
+                delete occupants[i];
+            }
+            delete[] occupants;
+        }
+
+    private:
+        int number;
+        int maxOccupants;
+        int occupantsCount;
+        Person** occupants;
+    };
+
+    class House {
+    public:
+        House(const string& address, int maxApartments) : address(new string(address)), maxApartments(maxApartments), apartmentsCount(0) {
+            apartments = new Apartment * [maxApartments];
+        }
+
+        // Конструктор копирования
+        House(const House& other) : address(new string(*other.address)), maxApartments(other.maxApartments), apartmentsCount(other.apartmentsCount) {
+            apartments = new Apartment * [maxApartments];
+            for (int i = 0; i < apartmentsCount; ++i) {
+                apartments[i] = new Apartment(*other.apartments[i]);
+            }
+        }
+
+        void addApartment(const Apartment& apartment) {
+            if (apartmentsCount < maxApartments) {
+                apartments[apartmentsCount] = new Apartment(apartment);
+                apartmentsCount++;
+            }
+        }
+
+        void display() const {
+            cout << "Домашний адрес: " << *address << endl;
+            for (int i = 0; i < apartmentsCount; ++i) {
+                apartments[i]->display();
+            }
+        }
+
+        // Деструктор
+        ~House() {
+            for (int i = 0; i < apartmentsCount; ++i) {
+                delete apartments[i];
+            }
+            delete[] apartments;
+            delete address;
+        }
+
+    private:
+        string* address;
+        int maxApartments;
+        int apartmentsCount;
+        Apartment** apartments;
+    };
+
+
+int main() 
+{
+    setlocale(LC_ALL, "Rus");
+    Person person1("Мария", 25);
+    Person person2("Евгений", 30);
+
+    Apartment apartment1(101, 2);
+    apartment1.addPerson(person1);
+    apartment1.addPerson(person2);
+
+    House house("ул. Ленина, 35", 1);
+    house.addApartment(apartment1);
+
+    House houseCopy = house; // Вызов конструктора копирования
+
+    house.display();
+    cout << "-------------------" << endl;
+    houseCopy.display();
+
+    return 0;
+}
+
+
---
+++ ДЗ №67_68 Классы (Дробь_Человек_Строка_Дом)/ДЗ №67_68 Классы (срок 27.04.23) (Человек).cpp
@@ -0,0 +1,165 @@
+﻿// ДЗ №67_68 Классы (срок 27.04.23) (Человек)
+//
+// Создать класс Человек.
+// Поля:
+//■ идентификационный номер;
+//■ фамилия;
+//■ имя;
+//■ отчество(для фамилии, имени и отчества память выделять динамически!);
+//■ дата рождения(рекомендуется создать дополнительный класс Дата(день, месяц, год);
+// Функции - члены:
+//■ конструктор с  параметрами идентификационный номер, фамилия, имя, отчество, дата рождения.
+//  В конструкторе использовать список инициализаторов полей класса;
+//■ конструктор по умолчанию.В конструкторе использовать делегирование конструктора;
+//■ конструктор копирования;
+//■ деструктор;
+//■ функцию - член для подсчета созданных экземпляров класса «Человек»;
+//■ сеттеры / геттеры для соответствующих полей класса;
+//■ вывод на экран информации о человеке
+//
+//
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Date {
+public:
+    Date(int day, int month, int year)
+        : day(day), month(month), year(year) {}
+
+    void print() const {
+        std::cout << day << "." << month << "." << year;
+    }
+
+private:
+    int day, month, year;
+};
+
+class Person {
+public:
+    // Конструктор с параметрами
+    Person(int id, const char* lastName, const char* firstName, const char* middleName, const Date& birthDate)
+        : id(id), birthDate(birthDate) {
+        this->lastName = new char[strlen(lastName) + 1];
+        strcpy_s(this->lastName, strlen(lastName) + 1, lastName);
+
+        this->firstName = new char[strlen(firstName) + 1];
+        strcpy_s(this->firstName, strlen(firstName) + 1, firstName);
+
+        this->middleName = new char[strlen(middleName) + 1];
+        strcpy_s(this->middleName, strlen(middleName) + 1, middleName);
+
+        personCount++; // Увеличение счетчика
+    }
+
+    // Конструктор по умолчанию
+    Person() : Person(0, "", "", "", Date(0, 0, 0)) {}
+
+    // Конструктор копирования
+    Person(const Person& other)
+        : id(other.id), birthDate(other.birthDate) {
+        lastName = new char[strlen(other.lastName) + 1];
+        strcpy_s(lastName, strlen(other.lastName) + 1, other.lastName);
+
+        firstName = new char[strlen(other.firstName) + 1];
+        strcpy_s(firstName, strlen(other.firstName) + 1, other.firstName);
+
+        middleName = new char[strlen(other.middleName) + 1];
+        strcpy_s(middleName, strlen(other.middleName) + 1, other.middleName);
+
+        personCount++; // Увеличение счетчика
+    }
+
+    // Деструктор
+    ~Person() {
+        delete[] lastName;
+        delete[] firstName;
+        delete[] middleName;
+        personCount--; // Уменьшение счетчика
+    }
+
+    // Функция-член для подсчета созданных экземпляров класса
+    static int getPersonCount() {
+        return personCount;
+    }
+
+    // Геттеры/сеттеры для полей
+    void setLastName(const char* lastName) {
+        delete[] this->lastName;
+        this->lastName = new char[strlen(lastName) + 1];
+        strcpy_s(this->lastName, strlen(lastName) + 1, lastName);
+    }
+
+    const char* getLastName() const {
+        return lastName;
+    }
+
+    void setFirstName(const char* firstName) {
+        delete[] this->firstName;
+        this->firstName = new char[strlen(firstName) + 1];
+        strcpy_s(this->firstName, strlen(firstName) + 1, firstName);
+    }
+
+    const char* getFirstName() const {
+        return firstName;
+    }
+
+    void setMiddleName(const char* middleName) {
+        delete[] this->middleName;
+        this->middleName = new char[strlen(middleName) + 1];
+        strcpy_s(this->middleName, strlen(middleName) + 1, middleName);
+    }
+
+    const char* getMiddleName() const {
+        return middleName;
+    }
+
+    void setBirthDate(const Date& birthDate) {
+        this->birthDate = birthDate;
+    }
+
+    const Date& getBirthDate() const {
+        return birthDate;
+    }
+
+    // Вывод на экран информации о человеке
+    void print() const {
+        cout << "ID: " << id << endl;
+        cout << "Фамилия: " << lastName << endl;
+        cout << "Имя: " << firstName << endl;
+        cout << "Отчество: " << middleName << endl;
+        cout << "Дата рождения: ";
+        birthDate.print();
+        cout << endl;
+    }
+
+private:
+    int id;
+    char* lastName;
+    char* firstName;
+    char* middleName;
+    Date birthDate;
+
+    static int personCount; // Статическая переменная для подсчета созданных экземпляров
+};
+
+int Person::personCount = 0; // Инициализация статической переменной
+
+int main() 
+{
+    setlocale(LC_ALL, "Rus");
+    Date birthDate(1, 8, 1990);
+
+    Person person1(1, "Иванов", "Петр", "Александрович", birthDate);
+    person1.print();
+
+    Person person2(person1);
+    person2.setFirstName("Андрей");
+    person2.print();
+
+    cout << "Счетчик количества человек: " << Person::getPersonCount() << endl;
+
+    return 0;
+}
